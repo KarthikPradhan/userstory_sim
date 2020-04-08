401bc44830f17de0637de0bce6298c27dbbf0f8b
karthik-grimm <k.pradhan@grimm-gastrobedarf.de>
2020-04-08 02:37:30 +0200

    [File Creation] Environment setup completed

--- file_generator.py
+++ file_generator.py
@@ -27,47 +27,69 @@ import numpy as np
 beamng = BeamNGpy('localhost', 64256, home=r'C:\\Deepak\\beamng-research_unlimited\\trunk' )
 scenario = Scenario('west_coast_usa', '{scenario}')
 
-ai = Vehicle('ai', model='coupe', licence='THE AI', colour='Green')
-scenario.add_vehicle(ai, pos=(-198.5, -164.189, 119.7), rot=(0, 0, -126.25))
+vut = Vehicle('vut', model='coupe', licence='VUT', colour='Green')
+scenario.add_vehicle(vut, pos=(-198.5, -164.189, 119.7), rot=(0, 0, -126.25))
 """
-    file_contents += environment_setup(test_case)
-    # if scenario == 't_intersection_w_obj':
-    #     file_contents += t_intersection_w_obj_fn(test_case)
+    dict_env = environment_setup(test_case)
+    file_contents += dict_env.get('file_content')
+    no_of_cars = dict_env.get('no_of_cars')
 
-    file_contents += """scenario.make(beamng)
+    file_contents += """
+scenario.make(beamng)
 bng = beamng.open(launch=True)
 bng.load_scenario(scenario)
 bng.start_scenario()
 
-ai.ai_set_mode('span')
-ai.ai_drive_in_lane(True)
-ai.ai_set_aggression(0.1)
+vut.ai_set_mode('span')
+vut.ai_drive_in_lane(True)
+vut.ai_set_aggression(0.1)
 
+"""
+
+    file_contents += define_cars_paths(no_of_cars)
+    file_contents += fetch_test_case_content(test_case)
+    file_contents += """
 while True:
     bng.step(60)
     """
     f.write(file_contents)
     f.close()
 
+
 def create_objs(n):
     i = 1
     objs = """"""
     while i <= n:
-        objs = f"""obj_{i} = StaticObject('obj_{i}', pos=(-140, -121.233, 119.586), rot=(0, 0, 55), scale=(1, 1, 1), shape='/levels/west_coast_usa/art/shapes/objects/barrierfence_folk.dae')
+        objs += f"""
+obj_{i} = StaticObject('obj_{i}', pos=(-140, -121.233, 119.586), rot=(0, 0, 55), scale=(1, 1, 1), shape='/levels/west_coast_usa/art/shapes/objects/barrierfence_folk.dae')
 scenario.add_object(obj_{i})
 """
         i += 1
     return objs
 
+
 def create_cars(n):
     i = 1
     cars = """"""
     while i <= n:
-        cars = f"""car_{i} = Vehicle('caa', model='etk800', licence='CAR {i}')
+        cars += f"""
+car_{i} = Vehicle('caa', model='etk800', licence='CAR {i}')
 scenario.add_vehicle(car_{i}, pos=(-140, -121.233, 119.586), rot=(0, 0, 55))
 """
         i += 1
     return cars
+
+
+def define_cars_paths(no_of_cars):
+    i = 1
+    cars_env = """"""
+    attrs = "{'pos': (-198.5, -164.189, 119.7), 'speed': 2000}"
+    while i <= no_of_cars:
+        cars_env += f"""car_{i}.ai_set_line([{attrs}])
+"""
+        i += 1
+    return cars_env
+
 
 def count_cars_objs(car_obj):
     car, obj = wn.synset('car.n.01'), wn.synset('object.n.01')
@@ -75,6 +97,7 @@ def count_cars_objs(car_obj):
     car_sim, obj_sim = car.path_similarity(car_obj_syn), obj.path_similarity(car_obj_syn)
     print('EVENT: ', car_obj, ', CAR SIMILARITY: ', car_sim, ', OBJ SIMILARITY: ', obj_sim)
     return 'car' if car_sim > obj_sim else 'obj'
+
 
 def environment_setup(test_case):
     lst_events = [e[1] for e in test_case]
@@ -85,11 +108,14 @@ def environment_setup(test_case):
         else:
             no_of_objs += 1
 
-    no_of_cars /= 2 # Quick hack to avoid unnecessary multiplication of cars by 2
+    no_of_cars /= 2  # Quick hack to avoid unnecessary multiplication of cars by 2
     print('CAR COUNT: ', no_of_cars, 'OBJ COUNT: ', no_of_objs)
 
-    file_contents = """car.ai_set_line([{'pos': (-198.5, -164.189, 119.7), 'speed': 2000}])"""
-    return create_objs(no_of_objs) + create_cars(no_of_cars)
+    return {'file_content': create_objs(no_of_objs) + create_cars(no_of_cars), 'no_of_cars': no_of_cars}
+
+
+def fetch_test_case_content(test_case):
+    pass
 
 
 create_file('t_intersection_w_obj', t_intersection_w_obj)
